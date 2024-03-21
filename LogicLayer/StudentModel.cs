62d951b08a097df5fb15425c9cbccb866f50b286
Alexandra Vegas <alexvegasdev@gmail.com>
2024-03-20 19:14:38 -0500

    cr

---
+++ LogicLayer/StudentModel.cs
@@ -0,0 +1,46 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class StudentModel
+    {
+        public Connection connection;
+
+        public StudentModel()
+        {
+            // obtaining connection instance
+            this.connection = Connection.getInstance();
+        }
+
+        // method to obtain students
+        public List<List<string>> getStudents()
+        {
+            return this.connection.getStudents();
+        }
+
+
+        // method to insert students
+        public void insertStudent(string primerNombre, string segundoNombre, string primerApellido, string segundoApellido,
+                          string telefono, string celular, string direccion, string gmail, DateTime fechaNacimiento,
+                          string observaciones, int nivelId)
+        {
+
+            // Llamar al método insertStudent de la instancia connection sin esperar un retorno.
+            this.connection.insertStudent(primerNombre, segundoNombre, primerApellido, segundoApellido,
+                                          telefono, celular, direccion, gmail, fechaNacimiento,
+                                          observaciones, nivelId);
+        }
+
+
+        // method to eliminate students
+        public void deleteStudent() { }
+
+        // method to update students
+        public void updateStudent() { }
+    }
+}
---
+++ PresentationLayer/FormListaEstudiante.xaml.cs
@@ -0,0 +1,69 @@
+﻿using LogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+    /// <summary>
+    /// Lógica de interacción para FormListaEstudiante.xaml
+    /// </summary>
+    public partial class FormListaEstudiante : Window
+    {
+        public FormListaEstudiante()
+        {
+            InitializeComponent();
+            StudentModel studentModel = new StudentModel();
+            foreach (var item in studentModel.getStudents())
+            {
+                var row = new
+                {
+                    Id = item[0],
+                    Primer_Nombre = item[1],
+                    Segundo_Nombre = item[2],
+                    Primer_Apellido = item[3],
+                    Segundo_Apellido = item[4],
+                    Telefono = item[5],
+                    Celular = item[6],
+                    Direccion = item[7],
+                    Gmail = item[8],
+                    Fecha_Nacimiento = item[9],
+                    Observaciones = item[10],
+                    Id_Nivel = item[11]
+                };
+
+                dgEstudiantes.Items.Add(row);
+            }
+        }
+
+        private void btnNuevoEstudiante_Click(object sender, RoutedEventArgs e)
+        {
+            FormNuevoEstudiante nuevoEstudiante = new FormNuevoEstudiante();
+            nuevoEstudiante.Show();
+            this.Hide();
+        }
+
+        private void btnEditarEstudiante_Click(object sender, RoutedEventArgs e)
+        {
+            //FormEditarEstudiante editarEstudiante = new FormEditarEstudiante();
+            //editarEstudiante.Show();
+            //this.Hide();
+        }
+
+        private void btnEliminarEstudiant_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+    }
+    
+}
