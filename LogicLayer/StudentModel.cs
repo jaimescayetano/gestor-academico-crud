a4b1284226d4eeba2df75a51dbef29faca6d2181
Jaimes Cayetano <jhonalexisjaimescayetano1@gmail.com>
2024-03-28 10:19:50 -0500

    folder organization

--- LogicLayer/StudentModel.cs
+++ LogicLayer/StudentModel.cs
@@ -13,34 +13,27 @@ namespace LogicLayer
 
         public StudentModel()
         {
-            // obtaining connection instance
             this.connection = Connection.getInstance();
         }
 
-        // method to obtain students
         public List<List<string>> getStudents()
         {
             return this.connection.getStudents();
         }
 
 
-        // method to insert students
         public void insertStudent(string primerNombre, string segundoNombre, string primerApellido, string segundoApellido,
                           string telefono, string celular, string direccion, string gmail, DateTime fechaNacimiento,
                           string observaciones, string nivelId)
         {
 
-            // Llamar al método insertStudent de la instancia connection sin esperar un retorno.
             this.connection.insertStudent(primerNombre, segundoNombre, primerApellido, segundoApellido,
                                           telefono, celular, direccion, gmail, fechaNacimiento,
                                           observaciones, nivelId);
         }
 
-
-        // method to eliminate students
         public void deleteStudent() { }
 
-        // method to update students
         public void updateStudent() { }
     }
 }
